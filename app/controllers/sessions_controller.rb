7096fbd47ca7fac8c10374b74ca53b00f23fad0f
Tan Wang Leng <tanwangleng@outlook.com>
2016-06-01 12:57:47 +0800

    Add session logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = user.find_by(matricno: params[:matricno])
+    if user and user.authenticate(params[:password])
+      sessions[:user_id] = user_id
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert:"Invalid Username or Password"
+    end
   end
 
   def destroy
