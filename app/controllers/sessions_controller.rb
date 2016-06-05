ce0e0a27750f6708c5ae6de8eae12f95b142a038
Tan Wang Leng <tanwangleng@outlook.com>
2016-06-05 21:23:32 +0800

    Fix typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(matricno: params[:matricno])
     if user and user.authenticate(params[:password])
-      sessions[:user_id] = user_id
+      session[:user_id] = user.id
       redirect_to admin_url
     else
       redirect_to login_url, alert:"Invalid Username or Password"
