fdcab1e1a2901a8ce0fea015398be9c7d1221000
initialshl <initials.hl@live.com>
2016-06-08 10:22:48 +0800

    Use bootstrap for collapsible header
    Need more work to make navbar-links stacked when size is xs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(matricno: params[:matricno])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
+      session[:user_matricNo] = user.matricNo
       # Redirect to tutorials/view
       redirect_to tutorials_view_url
     else
@@ -16,6 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    session[:user_matricNo] = nil
     redirect_to login_url, alert:"Successfully logged out"
   end
 end
