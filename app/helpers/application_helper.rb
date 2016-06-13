ade11a6345cb62a9adb7c3e45dd32917b2a7b0af
Tan Wang Leng <tanwangleng@outlook.com>
2016-06-13 14:58:36 +0800

    Refactor the number->day conversion method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def getDay(num)
+    case num
+    when 0
+      return "Sunday"
+    when 1
+      return "Monday"
+    when 2
+      return "Tuesday"
+    when 3
+      return "Wednesday"
+    when 4
+      return "Thursday"
+    when 5
+      return "Friday"
+    when 6
+      return "Saturday"
+    end
+
+    return "UNKNOWN_DATE"
+  end
 end
