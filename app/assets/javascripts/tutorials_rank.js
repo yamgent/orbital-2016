d6fc26f74b79f330ce762e3b41bc54c0a9891258
Tan Wang Leng <tanwangleng@outlook.com>
2016-06-17 13:12:25 +0800

    Update tutorials_rank.js (add more comments)

--- app/assets/javascripts/tutorials_rank.js
+++ app/assets/javascripts/tutorials_rank.js
@@ -2,9 +2,13 @@ function refresh_rank()
 {
   // to reset the ranks when we are done
   // sorting
-  
+
+  // for each item in the list
   $("#tutorial-list tr").each(function() {
+      // get the rank number
       rank = $(this).parent().children().index($(this)) + 1;
+
+      // modify the content to reflect the number
       $(this).find('[scope=row]').html(rank);
   });
 }
@@ -16,6 +20,8 @@ $(document).ready(function()
     // ourselves
     refresh_rank();
 
+    // make the list sortable, and refresh the rank
+    // numbers after a sort
     $("#tutorial-list tbody").sortable({stop: function() {
       refresh_rank();
     }});
