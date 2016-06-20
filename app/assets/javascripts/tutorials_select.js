5b50c223e66d2ae67abccf6a2cffb38363ce7e0f
initialshl <initials.hl@live.com>
2016-06-21 02:39:12 +0800

    Tutorial select saved preferences

--- app/assets/javascripts/tutorials_select.js
+++ app/assets/javascripts/tutorials_select.js
@@ -3,11 +3,21 @@ $(document).ready(function()
   // enable toggling after page has loaded
   // so that $(".card").data("periodId") is set
   $(".card").click(function() {
+
+    // this will also update the hidden field
+    // called "preferences" for submission later
+    var preferences = $("#preferences").val();
+
     if ($(this).hasClass("card-success")) {
       TimetablePreview().hidePeriodById($(this).data("periodId"));
+      preferences = preferences.replace($(this).data("periodId") + "_", "");
     } else {
       TimetablePreview().showPeriodById($(this).data("periodId"));
+      preferences = preferences.concat($(this).data("periodId") + "_");
     }
+
+    // update the hidden field
+    $("#preferences").val(preferences);
     
     $(this).toggleClass("card-inverse card-success", 200);
   });
