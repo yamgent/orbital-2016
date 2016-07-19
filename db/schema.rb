f21c08cce85c2a9ebb37d56b643618b89e07fb61
Tan Wang Leng <tanwangleng@outlook.com>
2016-07-19 11:49:30 +0800

    Add 'admin' field to users (for admin authentication)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713081749) do
+ActiveRecord::Schema.define(version: 20160719034722) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -73,8 +73,9 @@ ActiveRecord::Schema.define(version: 20160713081749) do
   create_table "users", force: :cascade do |t|
     t.string   "matricNo"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
   end
 
 end
