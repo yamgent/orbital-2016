0ef46b31b3a5c9b265d84e56581cd59789738330
Tan Wang Leng <tanwangleng@outlook.com>
2016-07-20 20:25:04 +0800

    DB: Cascade delete for User and Tutorial

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -3,6 +3,6 @@ class Tutorial < ActiveRecord::Base
 
   belongs_to :course
 
-  has_many :user_tutorial_selections
+  has_many :user_tutorial_selections, dependent: :destroy
   has_many :users, through: :user_tutorial_selections
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  has_many :user_course_selections
+  has_many :user_course_selections, dependent: :destroy
   has_many :courses, through: :user_course_selections
 
-  has_many :user_tutorial_selections
+  has_many :user_tutorial_selections, dependent: :destroy
   has_many :tutorials, through: :user_tutorial_selections
 end
