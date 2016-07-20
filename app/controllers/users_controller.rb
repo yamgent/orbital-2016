e72281949af3446ec9ff52581d5fb775ce36bd3b
Tan Wang Leng <tanwangleng@outlook.com>
2016-07-20 21:30:33 +0800

    Remove temporary code
    Variables no longer conflict, can be initialized safely.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_edit_user, only: [:show, :edit, :update, :destroy]
-  skip_before_action :initUser  # TODO: Temporary
 
   before_action :authorize_admin
 
